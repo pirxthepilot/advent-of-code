8d04daed0f2d31c6176b14f20fba509c31af2f3a
joon <pirxthepilot@users.noreply.github.com>
2023-12-09 01:36:28 -0800

    Day 5 p1, p2 in progress

---
+++ 2023/day05/solution.py
@@ -0,0 +1,120 @@
+import json
+import sys
+from typing import List
+
+
+FILE = "test.txt" if len(sys.argv) == 2 and sys.argv[1] == "test" else "input.txt"
+
+
+with open(FILE, "r") as f:
+    text = [t.strip() for t in f.readlines()]
+
+
+class Map:
+    def __init__(self):
+        self.maps = []
+
+    def add(self, line: str) -> None:
+        dest, src, counter = [int(i) for i in line.strip().split(" ")]
+        self.maps.append({
+            "dest": dest,
+            "src": src,
+            "counter": counter,
+        })
+    
+    def get(self, src: int) -> int:
+        for m in self.maps:
+            if m["src"] <= src <= m["src"] + m["counter"] - 1:
+                return m["dest"] + (src - m["src"])
+        return src
+
+
+class Almanac:
+    def __init__(self, text: str):
+        self.seeds = []
+        self.seed_soil = Map()
+        self.soil_fert = Map()
+        self.fert_water = Map()
+        self.water_light = Map()
+        self.light_temp = Map()
+        self.temp_humid = Map()
+        self.humid_loc = Map()
+
+        mode = ""
+        for line in text:
+            if not line:
+                continue
+
+            if ":" in line:
+                mode = line.split(":")[0]
+                if mode == "seeds":
+                    self._parse_seeds(line.split(":")[1])
+                continue
+            
+            if mode == "seed-to-soil map":
+                self.seed_soil.add(line)
+            elif mode == "soil-to-fertilizer map":
+                self.soil_fert.add(line)
+            elif mode == "fertilizer-to-water map":
+                self.fert_water.add(line)
+            elif mode == "water-to-light map":
+                self.water_light.add(line)
+            elif mode == "light-to-temperature map":
+                self.light_temp.add(line)
+            elif mode == "temperature-to-humidity map":
+                self.temp_humid.add(line)
+            elif mode == "humidity-to-location map":
+                self.humid_loc.add(line)
+        
+    def _parse_seeds(self, line: str) -> None:
+        for i in line.strip().split(" "):
+            self.seeds.append(int(i))
+    
+    def show(self) -> str:
+        output = {}
+        for attribute in (
+            "seed_soil",
+            "soil_fert",
+            "fert_water",
+            "water_light",
+            "light_temp",
+            "temp_humid",
+            "humid_loc",
+        ):
+            output[attribute] = getattr(self, attribute).maps
+        return json.dumps(output)
+    
+    def get_locs_from_seeds(self, range_mode=False) -> List[int]:
+        locs = []
+        if range_mode:
+            seeds = self.seeds
+        else:
+            seeds = self.seeds
+
+        for seed in seeds:
+            soil = self.seed_soil.get(seed)
+            fert = self.soil_fert.get(soil)
+            water = self.fert_water.get(fert)
+            light = self.water_light.get(water)
+            temp = self.light_temp.get(light)
+            humid = self.temp_humid.get(temp)
+            loc = self.humid_loc.get(humid)
+            print(f"Seed {seed} -> Soil {soil} -> Fert {fert} -> Water {water} -> Light {light}"
+                  f" -> Temp {temp} -> Humid {humid} -> Loc {loc}")
+            locs.append(loc)
+        return locs
+
+
+def s1(almanac: Almanac) -> int:
+    # return almanac.show()
+    return min(almanac.get_locs_from_seeds())
+
+
+def s2(almanac: Almanac) -> int:
+    # return almanac.show()
+    return min(almanac.get_locs_from_seeds(range_mode=True))
+
+
+almanac = Almanac(text)
+print(s1(almanac))
+print(s2(almanac))
